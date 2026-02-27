1c47bbac086f189f884d2be59f711631c83cb45a
MEER4 <mario.estrella.ing.software@gmail.com>
2026-02-26 22:26:48 -0400

    feat: complete alphabet levels for phonetics and tracing games

--- lib/constants/levels.ts
+++ lib/constants/levels.ts
@@ -11,87 +11,39 @@ export type GameLevel = {
 // Catálogo Centralizado de Niveles - Minijuego de Fonética
 export const ALL_PHONETICS_LEVELS: GameLevel[] = [
     // --- MUNDO 1: Las Vocales ---
-    {
-        id: 'lvl_1_a',
-        word: 'Abeja',
-        correctLetter: 'A',
-        missingIndex: 0,
-        options: ['O', 'A', 'E'],
-        imageIcon: '🐝',
-        world: 'Las Vocales'
-    },
-    {
-        id: 'lvl_2_e',
-        word: 'Elefante',
-        correctLetter: 'E',
-        missingIndex: 0,
-        options: ['I', 'U', 'E'],
-        imageIcon: '🐘',
-        world: 'Las Vocales'
-    },
-    {
-        id: 'lvl_3_i',
-        word: 'Isla',
-        correctLetter: 'I',
-        missingIndex: 0,
-        options: ['A', 'I', 'O'],
-        imageIcon: '🏝️',
-        world: 'Las Vocales'
-    },
-    {
-        id: 'lvl_4_o',
-        word: 'Oso',
-        correctLetter: 'O',
-        missingIndex: 0,
-        options: ['U', 'E', 'O'],
-        imageIcon: '🐻',
-        world: 'Las Vocales'
-    },
-    {
-        id: 'lvl_5_u',
-        word: 'Uva',
-        correctLetter: 'U',
-        missingIndex: 0,
-        options: ['A', 'U', 'I'],
-        imageIcon: '🍇',
-        world: 'Las Vocales'
-    },
+    { id: 'lvl_1_a', word: 'Abeja', correctLetter: 'A', missingIndex: 0, options: ['O', 'A', 'E'], imageIcon: '🐝', world: 'Las Vocales' },
+    { id: 'lvl_2_e', word: 'Elefante', correctLetter: 'E', missingIndex: 0, options: ['I', 'U', 'E'], imageIcon: '🐘', world: 'Las Vocales' },
+    { id: 'lvl_3_i', word: 'Isla', correctLetter: 'I', missingIndex: 0, options: ['A', 'I', 'O'], imageIcon: '🏝️', world: 'Las Vocales' },
+    { id: 'lvl_4_o', word: 'Oso', correctLetter: 'O', missingIndex: 0, options: ['U', 'E', 'O'], imageIcon: '🐻', world: 'Las Vocales' },
+    { id: 'lvl_5_u', word: 'Uva', correctLetter: 'U', missingIndex: 0, options: ['A', 'U', 'I'], imageIcon: '🍇', world: 'Las Vocales' },
 
     // --- MUNDO 2: Primeras Letras ---
-    {
-        id: 'lvl_6_m',
-        word: 'Mamá',
-        correctLetter: 'M',
-        missingIndex: 0,
-        options: ['P', 'M', 'S'],
-        imageIcon: '👩',
-        world: 'Primeras Letras'
-    },
-    {
-        id: 'lvl_7_p',
-        word: 'Pato',
-        correctLetter: 'P',
-        missingIndex: 0,
-        options: ['T', 'B', 'P'],
-        imageIcon: '🦆',
-        world: 'Primeras Letras'
-    },
-    {
-        id: 'lvl_8_s',
-        word: 'Sol',
-        correctLetter: 'S',
-        missingIndex: 0,
-        options: ['S', 'C', 'Z'],
-        imageIcon: '☀️',
-        world: 'Primeras Letras'
-    },
-    {
-        id: 'lvl_9_l',
-        word: 'Luna',
-        correctLetter: 'L',
-        missingIndex: 0,
-        options: ['R', 'L', 'N'],
-        imageIcon: '🌙',
-        world: 'Primeras Letras'
-    }
+    { id: 'lvl_6_m', word: 'Mamá', correctLetter: 'M', missingIndex: 0, options: ['P', 'M', 'S'], imageIcon: '👩', world: 'Primeras Letras' },
+    { id: 'lvl_7_p', word: 'Pato', correctLetter: 'P', missingIndex: 0, options: ['T', 'B', 'P'], imageIcon: '🦆', world: 'Primeras Letras' },
+    { id: 'lvl_8_s', word: 'Sol', correctLetter: 'S', missingIndex: 0, options: ['S', 'C', 'Z'], imageIcon: '☀️', world: 'Primeras Letras' },
+    { id: 'lvl_9_l', word: 'Luna', correctLetter: 'L', missingIndex: 0, options: ['R', 'L', 'N'], imageIcon: '🌙', world: 'Primeras Letras' },
+
+    // --- MUNDO 3: Letras Fuertes ---
+    { id: 'lvl_10_b', word: 'Búho', correctLetter: 'B', missingIndex: 0, options: ['V', 'D', 'B'], imageIcon: '🦉', world: 'Letras Fuertes' },
+    { id: 'lvl_11_c', word: 'Casa', correctLetter: 'C', missingIndex: 0, options: ['K', 'S', 'C'], imageIcon: '🏠', world: 'Letras Fuertes' },
+    { id: 'lvl_12_d', word: 'Dedo', correctLetter: 'D', missingIndex: 0, options: ['B', 'D', 'P'], imageIcon: '👆', world: 'Letras Fuertes' },
+    { id: 'lvl_13_f', word: 'Fuego', correctLetter: 'F', missingIndex: 0, options: ['F', 'P', 'V'], imageIcon: '🔥', world: 'Letras Fuertes' },
+    { id: 'lvl_14_g', word: 'Gato', correctLetter: 'G', missingIndex: 0, options: ['J', 'G', 'C'], imageIcon: '🐱', world: 'Letras Fuertes' },
+    { id: 'lvl_15_j', word: 'Jugo', correctLetter: 'J', missingIndex: 0, options: ['G', 'J', 'Y'], imageIcon: '🧃', world: 'Letras Fuertes' },
+    { id: 'lvl_16_r', word: 'Rana', correctLetter: 'R', missingIndex: 0, options: ['L', 'RR', 'R'], imageIcon: '🐸', world: 'Letras Fuertes' },
+    { id: 'lvl_17_t', word: 'Tren', correctLetter: 'T', missingIndex: 0, options: ['D', 'T', 'P'], imageIcon: '🚂', world: 'Letras Fuertes' },
+
+    // --- MUNDO 4: Letras Suaves ---
+    { id: 'lvl_18_h', word: 'Hielo', correctLetter: 'H', missingIndex: 0, options: ['I', 'J', 'H'], imageIcon: '🧊', world: 'Letras Suaves' },
+    { id: 'lvl_19_k', word: 'Koala', correctLetter: 'K', missingIndex: 0, options: ['C', 'Q', 'K'], imageIcon: '🐨', world: 'Letras Suaves' },
+    { id: 'lvl_20_n', word: 'Nube', correctLetter: 'N', missingIndex: 0, options: ['M', 'N', 'Ñ'], imageIcon: '☁️', world: 'Letras Suaves' },
+    { id: 'lvl_21_ny', word: 'Ñandú', correctLetter: 'Ñ', missingIndex: 0, options: ['N', 'Ñ', 'Y'], imageIcon: '🦤', world: 'Letras Suaves' },
+    { id: 'lvl_22_v', word: 'Vaca', correctLetter: 'V', missingIndex: 0, options: ['B', 'V', 'F'], imageIcon: '🐄', world: 'Letras Suaves' },
+    { id: 'lvl_23_y', word: 'Yoyo', correctLetter: 'Y', missingIndex: 0, options: ['LL', 'I', 'Y'], imageIcon: '🪀', world: 'Letras Suaves' },
+
+    // --- MUNDO 5: Letras Especiales ---
+    { id: 'lvl_24_q', word: 'Queso', correctLetter: 'Q', missingIndex: 0, options: ['K', 'C', 'Q'], imageIcon: '🧀', world: 'Letras Especiales' },
+    { id: 'lvl_25_w', word: 'Wifi', correctLetter: 'W', missingIndex: 0, options: ['V', 'W', 'U'], imageIcon: '📶', world: 'Letras Especiales' },
+    { id: 'lvl_26_x', word: 'Xilófono', correctLetter: 'X', missingIndex: 0, options: ['S', 'Z', 'X'], imageIcon: '🎹', world: 'Letras Especiales' },
+    { id: 'lvl_27_z', word: 'Zorro', correctLetter: 'Z', missingIndex: 0, options: ['S', 'C', 'Z'], imageIcon: '🦊', world: 'Letras Especiales' }
 ];
--- lib/constants/tracing-levels.ts
+++ lib/constants/tracing-levels.ts
@@ -7,102 +7,61 @@ export type TracingLevel = {
 };
 
 export const TRACING_LEVELS: TracingLevel[] = [
-    {
-        id: 'trace-a',
-        world: 'Estudio de Trazos',
-        name: 'Traza la A',
-        target: 'A',
-        guideColor: 'text-gray-200'
-    },
-    {
-        id: 'trace-a-min',
-        world: 'Estudio de Trazos',
-        name: 'Traza la a',
-        target: 'a',
-        guideColor: 'text-gray-200'
-    },
-    {
-        id: 'trace-e',
-        world: 'Estudio de Trazos',
-        name: 'Traza la E',
-        target: 'E',
-        guideColor: 'text-gray-200'
-    },
-    {
-        id: 'trace-e-min',
-        world: 'Estudio de Trazos',
-        name: 'Traza la e',
-        target: 'e',
-        guideColor: 'text-gray-200'
-    },
-    {
-        id: 'trace-i',
-        world: 'Estudio de Trazos',
-        name: 'Traza la I',
-        target: 'I',
-        guideColor: 'text-gray-200'
-    },
-    {
-        id: 'trace-i-min',
-        world: 'Estudio de Trazos',
-        name: 'Traza la i',
-        target: 'i',
-        guideColor: 'text-gray-200'
-    },
-    {
-        id: 'trace-o',
-        world: 'Estudio de Trazos',
-        name: 'Traza la O',
-        target: 'O',
-        guideColor: 'text-gray-200'
-    },
-    {
-        id: 'trace-o-min',
-        world: 'Estudio de Trazos',
-        name: 'Traza la o',
-        target: 'o',
-        guideColor: 'text-gray-200'
-    },
-    {
-        id: 'trace-u',
-        world: 'Estudio de Trazos',
-        name: 'Traza la U',
-        target: 'U',
-        guideColor: 'text-gray-200'
-    },
-    {
-        id: 'trace-u-min',
-        world: 'Estudio de Trazos',
-        name: 'Traza la u',
-        target: 'u',
-        guideColor: 'text-gray-200'
-    },
-    {
-        id: 'trace-m',
-        world: 'Estudio de Trazos',
-        name: 'Traza la M',
-        target: 'M',
-        guideColor: 'text-blue-100'
-    },
-    {
-        id: 'trace-m-min',
-        world: 'Estudio de Trazos',
-        name: 'Traza la m',
-        target: 'm',
-        guideColor: 'text-blue-100'
-    },
-    {
-        id: 'trace-p',
-        world: 'Estudio de Trazos',
-        name: 'Traza la P',
-        target: 'P',
-        guideColor: 'text-green-100'
-    },
-    {
-        id: 'trace-p-min',
-        world: 'Estudio de Trazos',
-        name: 'Traza la p',
-        target: 'p',
-        guideColor: 'text-green-100'
-    }
+    // --- VOCALES ---
+    { id: 'trace-a', world: 'Estudio de Trazos', name: 'Traza la A', target: 'A', guideColor: 'text-gray-200' },
+    { id: 'trace-a-min', world: 'Estudio de Trazos', name: 'Traza la a', target: 'a', guideColor: 'text-gray-200' },
+    { id: 'trace-e', world: 'Estudio de Trazos', name: 'Traza la E', target: 'E', guideColor: 'text-gray-200' },
+    { id: 'trace-e-min', world: 'Estudio de Trazos', name: 'Traza la e', target: 'e', guideColor: 'text-gray-200' },
+    { id: 'trace-i', world: 'Estudio de Trazos', name: 'Traza la I', target: 'I', guideColor: 'text-gray-200' },
+    { id: 'trace-i-min', world: 'Estudio de Trazos', name: 'Traza la i', target: 'i', guideColor: 'text-gray-200' },
+    { id: 'trace-o', world: 'Estudio de Trazos', name: 'Traza la O', target: 'O', guideColor: 'text-gray-200' },
+    { id: 'trace-o-min', world: 'Estudio de Trazos', name: 'Traza la o', target: 'o', guideColor: 'text-gray-200' },
+    { id: 'trace-u', world: 'Estudio de Trazos', name: 'Traza la U', target: 'U', guideColor: 'text-gray-200' },
+    { id: 'trace-u-min', world: 'Estudio de Trazos', name: 'Traza la u', target: 'u', guideColor: 'text-gray-200' },
+
+    // --- CONSONANTES ---
+    { id: 'trace-b', world: 'Estudio de Trazos', name: 'Traza la B', target: 'B', guideColor: 'text-red-100' },
+    { id: 'trace-b-min', world: 'Estudio de Trazos', name: 'Traza la b', target: 'b', guideColor: 'text-red-100' },
+    { id: 'trace-c', world: 'Estudio de Trazos', name: 'Traza la C', target: 'C', guideColor: 'text-blue-100' },
+    { id: 'trace-c-min', world: 'Estudio de Trazos', name: 'Traza la c', target: 'c', guideColor: 'text-blue-100' },
+    { id: 'trace-d', world: 'Estudio de Trazos', name: 'Traza la D', target: 'D', guideColor: 'text-green-100' },
+    { id: 'trace-d-min', world: 'Estudio de Trazos', name: 'Traza la d', target: 'd', guideColor: 'text-green-100' },
+    { id: 'trace-f', world: 'Estudio de Trazos', name: 'Traza la F', target: 'F', guideColor: 'text-yellow-100' },
+    { id: 'trace-f-min', world: 'Estudio de Trazos', name: 'Traza la f', target: 'f', guideColor: 'text-yellow-100' },
+    { id: 'trace-g', world: 'Estudio de Trazos', name: 'Traza la G', target: 'G', guideColor: 'text-purple-100' },
+    { id: 'trace-g-min', world: 'Estudio de Trazos', name: 'Traza la g', target: 'g', guideColor: 'text-purple-100' },
+    { id: 'trace-h', world: 'Estudio de Trazos', name: 'Traza la H', target: 'H', guideColor: 'text-pink-100' },
+    { id: 'trace-h-min', world: 'Estudio de Trazos', name: 'Traza la h', target: 'h', guideColor: 'text-pink-100' },
+    { id: 'trace-j', world: 'Estudio de Trazos', name: 'Traza la J', target: 'J', guideColor: 'text-orange-100' },
+    { id: 'trace-j-min', world: 'Estudio de Trazos', name: 'Traza la j', target: 'j', guideColor: 'text-orange-100' },
+    { id: 'trace-k', world: 'Estudio de Trazos', name: 'Traza la K', target: 'K', guideColor: 'text-red-100' },
+    { id: 'trace-k-min', world: 'Estudio de Trazos', name: 'Traza la k', target: 'k', guideColor: 'text-red-100' },
+    { id: 'trace-l', world: 'Estudio de Trazos', name: 'Traza la L', target: 'L', guideColor: 'text-blue-100' },
+    { id: 'trace-l-min', world: 'Estudio de Trazos', name: 'Traza la l', target: 'l', guideColor: 'text-blue-100' },
+    { id: 'trace-m', world: 'Estudio de Trazos', name: 'Traza la M', target: 'M', guideColor: 'text-green-100' },
+    { id: 'trace-m-min', world: 'Estudio de Trazos', name: 'Traza la m', target: 'm', guideColor: 'text-green-100' },
+    { id: 'trace-n', world: 'Estudio de Trazos', name: 'Traza la N', target: 'N', guideColor: 'text-yellow-100' },
+    { id: 'trace-n-min', world: 'Estudio de Trazos', name: 'Traza la n', target: 'n', guideColor: 'text-yellow-100' },
+    { id: 'trace-ny', world: 'Estudio de Trazos', name: 'Traza la Ñ', target: 'Ñ', guideColor: 'text-purple-100' },
+    { id: 'trace-ny-min', world: 'Estudio de Trazos', name: 'Traza la ñ', target: 'ñ', guideColor: 'text-purple-100' },
+    { id: 'trace-p', world: 'Estudio de Trazos', name: 'Traza la P', target: 'P', guideColor: 'text-pink-100' },
+    { id: 'trace-p-min', world: 'Estudio de Trazos', name: 'Traza la p', target: 'p', guideColor: 'text-pink-100' },
+    { id: 'trace-q', world: 'Estudio de Trazos', name: 'Traza la Q', target: 'Q', guideColor: 'text-orange-100' },
+    { id: 'trace-q-min', world: 'Estudio de Trazos', name: 'Traza la q', target: 'q', guideColor: 'text-orange-100' },
+    { id: 'trace-r', world: 'Estudio de Trazos', name: 'Traza la R', target: 'R', guideColor: 'text-red-100' },
+    { id: 'trace-r-min', world: 'Estudio de Trazos', name: 'Traza la r', target: 'r', guideColor: 'text-red-100' },
+    { id: 'trace-s', world: 'Estudio de Trazos', name: 'Traza la S', target: 'S', guideColor: 'text-blue-100' },
+    { id: 'trace-s-min', world: 'Estudio de Trazos', name: 'Traza la s', target: 's', guideColor: 'text-blue-100' },
+    { id: 'trace-t', world: 'Estudio de Trazos', name: 'Traza la T', target: 'T', guideColor: 'text-green-100' },
+    { id: 'trace-t-min', world: 'Estudio de Trazos', name: 'Traza la t', target: 't', guideColor: 'text-green-100' },
+    { id: 'trace-v', world: 'Estudio de Trazos', name: 'Traza la V', target: 'V', guideColor: 'text-yellow-100' },
+    { id: 'trace-v-min', world: 'Estudio de Trazos', name: 'Traza la v', target: 'v', guideColor: 'text-yellow-100' },
+    { id: 'trace-w', world: 'Estudio de Trazos', name: 'Traza la W', target: 'W', guideColor: 'text-purple-100' },
+    { id: 'trace-w-min', world: 'Estudio de Trazos', name: 'Traza la w', target: 'w', guideColor: 'text-purple-100' },
+    { id: 'trace-x', world: 'Estudio de Trazos', name: 'Traza la X', target: 'X', guideColor: 'text-pink-100' },
+    { id: 'trace-x-min', world: 'Estudio de Trazos', name: 'Traza la x', target: 'x', guideColor: 'text-pink-100' },
+    { id: 'trace-y', world: 'Estudio de Trazos', name: 'Traza la Y', target: 'Y', guideColor: 'text-orange-100' },
+    { id: 'trace-y-min', world: 'Estudio de Trazos', name: 'Traza la y', target: 'y', guideColor: 'text-orange-100' },
+    { id: 'trace-z', world: 'Estudio de Trazos', name: 'Traza la Z', target: 'Z', guideColor: 'text-red-100' },
+    { id: 'trace-z-min', world: 'Estudio de Trazos', name: 'Traza la z', target: 'z', guideColor: 'text-red-100' }
 ];
